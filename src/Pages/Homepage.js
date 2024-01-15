082eeed9b7a4c9b28b6d32b28db3290490f199d3
vimansha <vimanshamadushani88@gmail.com>
2024-01-15 23:12:14 +0530

    feat: start the first section

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import classes from "./Homepage.module.css";
 
-export default function Homepage() {
+export default function Homepage({ ...props }) {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div
+        className={`container-fluid ${classes.firstSection}`}
+        {...props}
+      ></div>
     </div>
   );
 }
