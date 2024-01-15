c6b1df3bc02a87d18d8bfeaff6cb6c0d9b9d4161
vimansha <vimanshamadushani88@gmail.com>
2024-01-15 23:37:53 +0530

    feat: heading and paragraph

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -4,10 +4,25 @@ import classes from "./Homepage.module.css";
 export default function Homepage({ ...props }) {
   return (
     <div>
-      <div
-        className={`container-fluid ${classes.firstSection}`}
-        {...props}
-      ></div>
+      <div className={`container-fluid ${classes.firstSection}`} {...props}>
+        <div className="container">
+          <div
+            className={`row justify-content-left ${classes.col1}`}
+            {...props}
+          >
+            <div className="col-6">
+              <h1 className={classes.heading}>
+                Meet Density, the visonary landing Page.
+              </h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Ducimus obcaecati temporibus voluptatem unde sunt aliquid esse
+                explicabo officiis praesentium aut?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
