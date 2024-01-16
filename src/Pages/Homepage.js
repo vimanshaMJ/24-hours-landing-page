4013835330d894d959f607350fc3d7f2ee653462
vimansha <vimanshamadushani88@gmail.com>
2024-01-16 11:12:18 +0530

    feat: start the second section

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -22,10 +22,22 @@ export default function Homepage({ ...props }) {
           </div>
         </div>
       </div>
-      <div
-        className={`container-fluid ${classes.secondSection}`}
-        {...props}
-      ></div>
+      <div className={`container-fluid ${classes.secondSection}`} {...props}>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-10" style={{ border: "solid" }}>
+              <h1>Density isn't A.I, Its the result of human intelligence.</h1>
+            </div>
+            <div className="col-8" style={{ border: "solid" }}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                quam sint animi cum sed ullam libero consectetur! Voluptatibus,
+                pariatur fuga modi voluptate vel labore odit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
